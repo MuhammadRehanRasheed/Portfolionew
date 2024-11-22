e4ff98b4840c6ec2d614090b83ef5f1e20b3c852
chrehan101 <adem@devopsifi.com>
2024-11-22 20:28:57 +0500

    initial commit

---
+++ src/components/Nevbar.jsx
@@ -0,0 +1,24 @@
+import { FaLinkedin } from 'react-icons/fa6'
+import { FaGithub } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa6'
+
+export const NevBar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className='flex flex-shrink-0 items-center'>
+      <h1 className='text-4xl'>MRR</h1>
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <a href="https://linkedin.com/in/muhammad-rehan-rasheed" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/MuhammadRehanRasheed" target="_blank" rel="noopener noreferrer">
+        <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/rehan_rasheed_chaudhary" target="_blank" rel="noopener noreferrer">
+        <FaInstagram />
+        </a>
+      </div>
+    </nav>
+  )
+}
---
+++ src/constants/index.js
@@ -0,0 +1,84 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+import project5 from "../assets/projects/project-5.png";
+
+export const HERO_CONTENT = `Hi, I'm Muhammad Rehan Rasheed, a passionate Software Engineering student with hands-on experience in web development and graphic design. My journey has led me through internships in WordPress and full-stack development, equipping me with diverse technical skills and a keen eye for design. My goal is to make an impact as a Full-Stack JavaScript Engineer, contributing to innovative projects in a forward-thinking organization.`;
+
+export const ABOUT_TEXT = `As a final-year Software Engineering student at the Virtual University of Pakistan, I've built a solid foundation in programming, software development, and project management. Alongside my academic pursuits, I've gained practical experience through internships in graphic design, WordPress development, and full-stack JavaScript engineering. My technical skills span front-end and back-end development, leveraging tools like React.js, Next.js, and Vue with Vuetify, while also having expertise in the Adobe Suite for graphic design. My vision is to join a team where I can contribute innovative solutions, collaborate effectively, and continue developing my skills in a professional setting.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Full Stack Developer (Internship)",
+    company: "CodeQuest, Lahore (3 months)",
+    description: `As an intern, I have developed several websites to enhance my skills and build a strong portfolio. These projects include a website showcasing my expectations and goals for the internship, an interactive quiz application, a site demonstrating creative transitions and animations, and a personal portfolio website. Each project reflects my dedication to experimenting with and mastering various technologies, including front-end frameworks, responsive design techniques, and animation libraries. These hands-on experiences have allowed me to expand my technical expertise and demonstrate my ability to deliver functional and visually appealing web applications.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2023 - 2024",
+    role: "Junior Web Developer And IT Trainer",
+    company: "Helpline Org Pk, Lahore (3 months)",
+    description: `As a Junior Web Developer, I assist in building and maintaining web applications, collaborate on feature implementation, and troubleshoot technical issues, In my role as an IT Trainer, I teach fundamental IT concepts and web development skills, create educational materials, and support learners to ensure effective understanding.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "WordPress Developer (Internship)",
+    company: "WebFoxes, Lahore (3 months)",
+    description: `Created new blog sites and web stores using WordPress, managed admin panels and published new articles on existing websites, update listings of different products.`,
+    technologies: ["WordPress", "Elementor", " WooCommerce", "UpdraftPlus"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Graphic Designer (Internship)",
+    company: "Benchmark Studio, Lahore (6 months)",
+    description: `Implemented knowledge gained from the Diploma in Web & Graphic Designing Executed tasks in error-correction, white-balance, re- generation, and photo enhancement using Photoshop of property shoots.`,
+    technologies: ["Photoshop", "Canva", "Lightroom", "Coreldraw"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "NFT Marketplace Aggregator",
+    image: project1,
+    description:
+      "My project focuses on the development of an NFT Marketplace Aggregator Web Application. In an era of unprecedented growth in the NFT industry, our project aims to address the evolving needs of NFT collectors and traders by providing a comprehensive, user-friendly platform for trading NFTs.",
+      technologies: ["HTML", "CSS", "Vue.js", "Node.js", "Express", "MongoDB"],
+  },
+  {
+    title: "Quiz App",
+    image: project2,
+    description:
+      "An interactive quiz application designed to provide a seamless experience for users, featuring functionalities such as question creation, timed quizzes, and instant feedback. The app includes a progress tracking system, allowing users to save their progress and resume later. At the end of each quiz, a detailed performance report is displayed, showcasing scores and correct answers, helping users gauge their understanding effectively. The user-friendly interface and responsive design ensure accessibility across devices, making it an engaging tool for learning and assessment.",
+    technologies: ["HTML", "CSS", "Bootstrap 5", "React.js"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap" ,"Tailwindcss"],
+  },
+  {
+    title: "Creative Transitions Showcase Website",
+    image: project4,
+    description:
+      "This project is a showcase of modern web transitions and animations, providing a visually engaging user experience. The website serves as a demonstration of various transition effects, animations, and layouts.",
+    technologies: ["HTML", "CSS", "React.js", "CSS Animations", "Material UI"],
+  },
+  {
+    title: "Internship Vision Website for Qode Quest",
+    image: project5,
+    description:
+      "Created as a personal project, this website represents my goals and expectations for my internship experience at Qode Quest. It demonstrates my enthusiasm and vision for the role, as well as my desire to contribute to innovative projects as part of the Qode Quest team.",
+    technologies: ["HTML", "CSS", "React.js", "CSS Animations", "Bootstrap"],
+  },
+];
+
+export const CONTACT = {
+  address: "Lahore, Punjab, Pakistan",
+  phoneNo: "0313-4821179",
+  email: "chrehan101@gmail.com",
+};
